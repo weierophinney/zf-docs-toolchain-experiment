525d950e5a48bdcadf687f887a4798e832af8d99
Matthew Weier O'Phinney <matthew@zend.com>
2016-01-26 10:11:20 -0600

    Fixes for img-responsive injection
    `iterator_apply` requires that the callback `return true` in order to iterate to
    the next item. Additionally, using `RegexIterator` obviates the need for a
    `FilterIterator` extension.

--- doc/img_responsive.php
+++ doc/img_responsive.php
@@ -1,49 +1,38 @@
-<?php // @codingStandardsIgnoreFile
+<?php
 /**
- * Swaps the generated HTML for hand-crafted HTML in the landing page.
+ * Adds `class="img-responsive"` to images in generated HTML files.
  *
  * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
  * @copyright Copyright (c) 2016 Zend Technologies USA Inc. (http://www.zend.com)
  */
 
 $rdi = new RecursiveDirectoryIterator(__DIR__ . '/html');
-$rii = new RecursiveIteratorIterator($rdi);
-$files = new HtmlFilter($rii);
+$rii = new RecursiveIteratorIterator($rdi, RecursiveIteratorIterator::SELF_FIRST);
+$files = new RegexIterator($rii, '/\.html$/', RecursiveRegexIterator::GET_MATCH);
 
 $process = function () use ($files) {
-    $file = $files->current()->getRealPath();
+    $fileInfo = $files->getInnerIterator()->current();
+    if (! $fileInfo->isFile()) {
+        return true;
+    }
+
+    if ($fileInfo->getBasename('.html') === $fileInfo->getBasename()) {
+        return true;
+    }
+
+    $file = $fileInfo->getRealPath();
     $html = file_get_contents($file);
-    if (! preg_match('#\<p\>\<img alt\="[^"]*" src\="[^"]+" \/\>\<\/p\>#s', $html)) {;
-        return;
+    if (! preg_match('#<p><img alt="[^"]*" src="[^"]+" \/><\/p>#s', $html)) {
+        return true;
     }
     $html = preg_replace(
-        '#(\<p\>\<img alt\="[^"]*" src\="[^"]+" )(\/\>\<\/p\>)#s',
+        '#(<p><img alt="[^"]*" src="[^"]+" )(\/><\/p>)#s',
         '$1class="img-responsive"$2',
         $html
     );
     file_put_contents($file, $html);
+
+    return true;
 };
 
 iterator_apply($files, $process);
-
-class HtmlFilter extends FilterIterator
-{
-    public function accept()
-    {
-        $file = $this->getInnerIterator()->current();
-
-        if (! $file instanceof SplFileInfo) {
-            return false;
-        }
-
-        if (! $file->isFile()) {
-            return false;
-        }
-
-        if ($file->getBasename('.html') === $file->getBasename()) {
-            return false;
-        }
-
-        return true;
-    }
-}
